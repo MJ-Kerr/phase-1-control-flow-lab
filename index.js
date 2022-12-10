fe5618e8b6281f91c37be114552770565dfef96d
Miles Kerr <m.kerr.professional@gmail.com>
2022-12-10 16:53:20 -0600

    commit tree c0d619884bda520e87ab4ecba3d50ef750e821bb

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+  if (ride <= 400){
+    return "This one is on me!"
+  }
+    else if (ride >= 400 && ride <= 2000){
+      return 'That will be twenty bucks.'
+    }
+    else if (ride >= 2000 && ride < 2500) {
+      return 'I will gladly take your thirty bucks.'
+  }
+    else if (ride >= 2500){
+      return 'No can do.'
+    }
+}
+function ternaryCheckCity(NYC){
+    return NYC == "NYC" ? 'Ok, sounds good.' : 'No go.';
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(Tips){
+  switch (Tips) {
+    case 'generous':
+      return 'Thank you so much.'
+    case 'not as generous':
+      return 'Thank you.'
+    default: 
+      return 'Bye.'
+  }
 }
